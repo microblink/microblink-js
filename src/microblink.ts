c78022dfa1bb4d09ccfe34ab250e62d31f3944bb
Matija Stepanic <stepanic@italk.hr>
2018-09-14 16:20:48 +0200

    Default init method created

--- src/microblink.ts
+++ src/microblink.ts
@@ -1,4 +1,12 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class DummyClass {}
+export default class Microblink {
+  public getData(): boolean {
+    return true
+  }
+
+  public init(): void {
+    //
+  }
+}
--- test/microblink.test.ts
+++ test/microblink.test.ts
@@ -1,4 +1,4 @@
-import DummyClass from '../src/microblink'
+import Microblink from '../src/microblink'
 
 /**
  * Dummy test
@@ -8,7 +8,16 @@ describe('Dummy test', () => {
     expect(true).toBeTruthy()
   })
 
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  it('Microblink class is instantiable', () => {
+    expect(new Microblink()).toBeInstanceOf(Microblink)
+  })
+
+  it('Microblink class has method getData', () => {
+    expect(new Microblink().getData).toBeTruthy()
+    expect(new Microblink().getData()).toBe(true)
+  })
+
+  it('Microblink class has method init', () => {
+    expect(new Microblink().init).toBeTruthy()
   })
 })
