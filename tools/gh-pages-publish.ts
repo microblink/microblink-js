6fdf9a06539f70f147c2dc4b63b89ded96c64960
Matija Stepanic <stepanic@italk.hr>
2018-09-19 10:56:32 +0200

    Updated tools/gh-pages-publish

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -14,8 +14,26 @@ if (typeof pkg.repository === "object") {
 }
 
 let parsedUrl = url.parse(repoUrl)
+
+console.log('parsedUrl', parsedUrl);
+
 let repository = (parsedUrl.host || "") + (parsedUrl.path || "")
+let deployUrl = repository;
+
+console.log('repository', repository);
+
 let ghToken = process.env.GH_TOKEN
+
+
+if (ghToken) {
+  if (deployUrl.startsWith('git@')) {
+    deployUrl = 'https://' + ghToken + '@' + repository.substring(4);
+  }
+}
+
+console.log('ghToken', ghToken);
+
+console.log('deployUrl', deployUrl);
 
 echo("Deploying docs!!!")
 cd("docs")
@@ -26,6 +44,6 @@ exec('git config user.name "Matija Stepanic"')
 exec('git config user.email "matija.stepanic@microblink.com"')
 exec('git commit -m "docs(docs): update gh-pages"')
 exec(
-  `git push --force --quiet "${repository}" master:gh-pages`
+  `git push --force --quiet "${deployUrl}" master:gh-pages`
 )
 echo("Docs deployed!!")
