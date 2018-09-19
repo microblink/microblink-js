2ae0aa36cbbb27a11a9dc286b6e69dcce92054bb
Matija Stepanic <stepanic@italk.hr>
2018-09-19 11:24:07 +0200

    fix: Fixed docs deploy script to works from CI

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -15,25 +15,21 @@ if (typeof pkg.repository === "object") {
 
 let parsedUrl = url.parse(repoUrl)
 
-console.log('parsedUrl', parsedUrl);
-
 let repository = (parsedUrl.host || "") + (parsedUrl.path || "")
 let deployUrl = repository;
 
-console.log('repository', repository);
-
 let ghToken = process.env.GH_TOKEN
-
 
 if (ghToken) {
   if (deployUrl.startsWith('git@')) {
     deployUrl = 'https://' + ghToken + '@' + repository.substring(4);
+    deployUrl = deployUrl.replace(/\/$/, "");
   }
 }
 
-console.log('ghToken', ghToken);
+const deployCmd = `git push --force --quiet '${deployUrl}' master:gh-pages`;
 
-console.log('deployUrl', deployUrl);
+console.log('deployCmd', deployCmd);
 
 echo("Deploying docs!!!")
 cd("docs")
@@ -44,6 +40,6 @@ exec('git config user.name "Matija Stepanic"')
 exec('git config user.email "matija.stepanic@microblink.com"')
 exec('git commit -m "docs(docs): update gh-pages"')
 exec(
-  `git push --force --quiet "${deployUrl}" master:gh-pages`
+  deployCmd
 )
 echo("Docs deployed!!")
