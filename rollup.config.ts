586136076f8ddb97677f4862dd8afaabc9988a70
Dragan Bezak <dragan.bezak@microblink.com>
2018-11-19 21:59:01 +0100

    included es5 version of the bundle

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,6 +7,7 @@ import json from 'rollup-plugin-json'
 import { terser } from "rollup-plugin-terser";
 import copy from 'rollup-plugin-cpy'
 import html from 'rollup-plugin-html'
+import babel from 'rollup-plugin-babel'
 
 const pkg = require('./package.json')
 
@@ -154,6 +155,69 @@ export default [
           collapseWhitespace: true,
           collapseBooleanAttributes: true,
           conservativeCollapse: true,
+          minifyCSS: true
+        }
+      }),
+      // Copy other non TypeScript (JavaScript) dependencies to `dist`
+      copy({
+        files: ['src/ui/*'],
+        dest: 'dist/lib/ui',
+        options: {
+          verbose: true
+        }
+      })
+    ],
+  },
+  {
+    input: `src/${camelCase(libraryName)}.UI.ts`,
+    output: [
+      { file: 'dist/' + camelCase(libraryName) + '.es5.min.js', name: libraryName, format: 'iife', sourcemap: false }
+    ],
+    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+    external: [],
+    watch: {
+      include: 'src/**',
+    },
+    plugins: [
+      // Allow json resolution
+      json(),
+      // Compile TypeScript files
+      typescript({ useTsconfigDeclarationDir: true }),
+      // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+      commonjs(),
+      // Allow node_modules resolution, so you can use 'external' to control
+      // which external modules to include in the bundle
+      // https://github.com/rollup/rollup-plugin-node-resolve#usage
+      resolve(),
+      babel({
+        exclude: 'node_modules/**', // only transpile our source code
+        babelrc: false,
+        presets: ["@babel/preset-env"],
+        runtimeHelpers: true,
+        plugins: [
+          ["@babel/plugin-transform-runtime", { helpers: true, corejs: 2 }],
+          "transform-custom-element-classes"
+        ]
+      }),
+      // Minify library
+      terser({
+        output: {
+          comments: function(node, comment) {
+            var text = comment.value;
+            var type = comment.type;
+            if (type === "comment2") {
+              // multiline comment
+              return /@preserve|@license|@cc_on/i.test(text);
+            }
+          }
+        }
+      }),
+      html ({
+        include: 'src/**/*.html',
+        htmlMinifierOptions: {
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          conservativeCollapse: false,
           minifyCSS: true
         }
       }),
